e2f259d763a9d482cd20c5ac29bb37f093fda865
Ayush Katiyar <ayushkatiyar096@gmail.com>
2021-10-09 17:16:26 +0530

    Add files via upload

---
+++ src/components/right/right.js
@@ -0,0 +1,15 @@
+function Right(props){
+ return (<div className="gr-genres">
+ <ul>
+     <li>
+         <span>GENRES</span>
+     </li>
+     <li><a href="#">History</a><span className="fl-right">12 users</span></li>
+     <li><a href="#">NonFiction</a><span className="fl-right">8 users</span></li>
+     <li><a href="#">War</a><span className="fl-right">12 users</span></li>
+     <li><a href="#">War {'>'} World War II</a><span className="fl-right">7 users</span></li>
+     <li><a href="#">War {'>'} Military Fiction </a><span className="fl-right">11 users</span></li>
+ </ul>
+</div>);
+}
+export default Right;
---
+++ src/constants.js
@@ -0,0 +1,14 @@
+const constants = {
+    p1 :`May 1940: The world is stunned as Hitler's forces invade France with a devastating blitzkrieg aimed at Paris. Within weeks, the French government has collapsed, and the City of Lights, revered for its carefree lifestyle, intellectual freedom, and love
+    of liberty, has fallen under Nazi control--perhaps forever.`,
+     p2 :`As the Germans ruthlessly crush all opposition, a patriotic band of Parisians known as the Resistance secretly rise up to fight back. But these young men and woman cannot do it alone. Over 120,000 Parisians die under German occupation. Countless more
+    are tortured in the city's Gestapo prisons and sent to death camps. The longer the Nazis hold the city, the greater the danger its citizens face. As the armies of America and Great Britain prepare to launch the greatest invasion
+    in history, the spies of the Resistance risk all to ensure the Germans are defeated and Paris is once again free.`,
+     p3:`The players holding the fate of Paris in their hands are some of the biggest historical figures of the era: Winston Churchill, Franklin Roosevelt, General George S. Patton, and the exiled French general Charles de Gaulle, headquartered in London's Connaught
+    Hotel. From the fall of Paris in 1940 to the race for Paris in 1944, this riveting, page-turning drama unfolds through their decisions--for better and worse. Taking Paris is history told at a breathtaking pace, a sprawling yet
+    intimate saga of heroism, desire, and personal sacrifice for all that is right.`,
+     pageHeading : `Taking Paris: The Epic Battle for the City of Lights`
+
+}
+ 
+export {constants}
