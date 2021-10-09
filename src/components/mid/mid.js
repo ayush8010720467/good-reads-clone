d2fee870ac1a7b5772a0784a9e472679316ad438
Ayush Katiyar <ayushkatiyar096@gmail.com>
2021-10-09 17:31:52 +0530

    Add files via upload

---
+++ src/components/mid/mid.js
@@ -0,0 +1,16 @@
+import Description from "../description";
+function Mid(props){
+    return (
+//         <div className="gr-desc">
+//     <Heading2 text={constants.pageHeading}></Heading2> 
+//     <Author></Author>
+//     <div className="description">
+//         <Paragraph text={constants.p1}></Paragraph>
+//         <Paragraph text={constants.p2}></Paragraph>
+//         <Paragraph text={constants.p3}></Paragraph>
+//     </div>
+// </div>
+        <Description></Description>
+);
+}
+export default Mid;
