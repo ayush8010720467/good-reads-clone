294803a2f935bd7bc7157bf74a89df42c5df332f
Ayush Katiyar <ayushkatiyar096@gmail.com>
2021-10-10 14:30:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Left from './components/left/left';
+import Mid from './components/mid/mid';
+import Right from './components/right/right';
+function App() {
+  return (
+    <>
+    <div className="body-wrap">
+        <div className="goodreads-wrap">
+            <Left></Left>
+            <Mid></Mid>
+            <Right></Right>
+        </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
