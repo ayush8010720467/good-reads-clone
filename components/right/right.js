521ace8bf5f03d9777d33c8a9e66888e5dc09b35
Ayush Katiyar <ayushkatiyar096@gmail.com>
2021-10-10 18:50:16 +0530

    Add files via upload

---
+++ components/right/right.js
@@ -0,0 +1,26 @@
+import { constants } from "../../constants";
+import Genere from "../genere";
+import Image from "../image";
+import {useEffect, useState} from 'react';
+function Right(props){
+    const getGenere = ()=>{
+        if(props.genre){
+            return  props.genre.map((elem, i)=>{
+                return <li key={i}><Genere gnere={elem} userNumber={i}></Genere></li>
+            })
+        } else{
+            return <Image src={constants.loader} ></Image>
+        }
+        
+    }
+    // const [generes, updategeneres] = useState(props.genere)
+ return (<div className="gr-genres">
+ <ul>
+     <li>
+         <span>GENRES</span>
+     </li>
+     {getGenere()}
+ </ul>
+</div>);
+}
+export default Right;
---
+++ env.js
@@ -0,0 +1,2 @@
+const base_url = 'https://lxkdtnbp26.execute-api.ap-south-1.amazonaws.com'
+export {base_url}
